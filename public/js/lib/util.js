1eaa9c6f2ed5f5a1243e5b83458aef37158ad9fb
miquelg <miquel.garcia@gmail.com>
2011-04-24 09:56:40 +0000

    Some code added

---
+++ public/js/lib/util.js
@@ -0,0 +1,9 @@
+inherits = function (ctor, superCtor) {
+    ctor.super_ = superCtor;
+    ctor.prototype = Object.create(superCtor.prototype, {
+        constructor: {
+            value: ctor,
+            enumerable: false
+        }
+    });
+};
